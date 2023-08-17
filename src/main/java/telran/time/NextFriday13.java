2efd960d410c95cd2590fdeecd3fb46e6bf37077
Anatoly <anatolyavbme@gmail.com>
2023-08-17 19:47:01 +0300

    HW24

---
+++ src/main/java/telran/time/NextFriday13.java
@@ -0,0 +1,38 @@
+package telran.time;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.UnsupportedTemporalTypeException;
+
+public class NextFriday13 implements TemporalAdjuster {
+
+    @Override
+    public Temporal adjustInto(Temporal temporal) {
+        if (!temporal.isSupported(ChronoUnit.YEARS)) {
+            throw new UnsupportedTemporalTypeException("must support years");
+        }
+
+        if (temporal.get(ChronoField.DAY_OF_MONTH) == 13) {
+            return temporal = monthPlus(temporal);
+        }
+        ;
+        do {
+            temporal = temporal.plus(1, ChronoUnit.DAYS);
+        } while (temporal.get(ChronoField.DAY_OF_MONTH) != 13);
+        if (temporal.get(ChronoField.DAY_OF_WEEK) != 5) {
+            temporal = monthPlus(temporal);
+        }
+
+        return temporal;
+    }
+
+    private Temporal monthPlus(Temporal temporal) {
+        do {
+            temporal = temporal.plus(1, ChronoUnit.MONTHS);
+        } while (temporal.get(ChronoField.DAY_OF_WEEK) != 5);
+        return temporal;
+    }
+}
